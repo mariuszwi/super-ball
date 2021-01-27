1b6482db889a9aebc075a3a54ff7ce7696d3df3b
Mariusz Wiśniewski <kontakt@wisniewskimariusz.com>
2021-01-27 22:02:54 +0100

    Paddle controll
    Added paddle keyboard controll.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,15 +5,35 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
-        const ball = this.add.circle(600, 250, 5, 0xffffff, 1);
-        const paddle = this.add.rectangle(400, 470, 60, 10, 0xffffff);
+        const ball  = this.add.circle(600, 250, 5, 0xffffff, 1);
+        this.paddle = this.add.rectangle(400, 470, 60, 10, 0xffffff);
         
         this.physics.add.existing(ball);
-        this.physics.add.existing(paddle, true);
-        this.physics.add.collider(ball, paddle);
-        
+        this.physics.add.existing(this.paddle, true);
+        this.physics.add.collider(ball, this.paddle);
+
         ball.body.setVelocity(-200, 200);
         ball.body.setCollideWorldBounds(true, 1, 1);
         ball.body.setBounce(1, 1);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if(this.cursors.left.isDown) {
+            if(this.paddle.x < 0) {
+                return;
+            }
+
+            this.paddle.x -= 10;
+            this.paddle.body.updateFromGameObject();
+        } else if( this.cursors.right.isDown) {
+            if(this.paddle.x > 800) {
+                return;
+            }
+
+            this.paddle.x += 10;
+            this.paddle.body.updateFromGameObject();
+        }
     }
 }
